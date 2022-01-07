33559a270aae37d0608e332544cf56eebcc42cd5
Michał "phoe" Herda <phoe@disroot.org>
2022-01-07 19:31:00 +0100

    More MOP wizardry

--- src/always-bound-class.lisp
+++ src/always-bound-class.lisp
@@ -9,8 +9,31 @@
                                 (superclass standard-class))
   t)
 
+(defclass always-bound (standard-object) ())
+
+(defmethod shared-initialize :around ((class always-bound-class) slot-names
+                                      &rest rest &key direct-superclasses)
+  (apply #'call-next-method
+         class slot-names
+         :direct-superclasses
+         (append (remove (find-class 'standard-object) direct-superclasses)
+                 (list (find-class 'always-bound)))
+         rest))
+
+(defun ensure-slot-bound (instance slotd)
+  (let ((name (slot-definition-name slotd)))
+    (unless (slot-boundp instance name)
+      (setf (slot-value instance name) (slot-value instance name)))))
+
+(defun ensure-slots-bound (instance)
+  (dolist (slotd (class-slots (class-of instance)))
+    (ensure-slot-bound instance slotd)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; SLOT-MAKUNBOUND
+
 (defmethod slot-makunbound-using-class
-    ((class always-bound-class) object slotd)
+    ((class always-bound-class) (object always-bound) slotd)
   (if (not (slot-boundp-using-class class object slotd))
       (call-next-method)
       (let* ((old-value (slot-value-using-class class object slotd)))
@@ -22,33 +45,68 @@
             (setf (slot-value-using-class class object slotd) old-value))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; ALWAYS-BOUND-CLASS SHARED-INITIALIZE
+;;; INITIALIZE-INSTANCE
 
-(defun ensure-slots-bound (instance)
-  (dolist (slot (class-slots (class-of instance)))
-    (let ((name (slot-definition-name slot)))
-      (unless (slot-boundp instance name)
-        (setf (slot-value instance name)
-              (slot-value instance name))))))
+(defmethod initialize-instance :after ((instance always-bound) &key)
+  (ensure-slots-bound instance))
 
-(defparameter *always-bound-initialize-mf*
-  (compile nil (make-method-lambda
-                (class-prototype (find-class 'standard-generic-function))
-                (class-prototype (find-class 'standard-method))
-                `(lambda (instance slot-names &rest args)
-                   (declare (ignore slot-names args))
-                   (ensure-slots-bound instance))
-                nil)))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; REINITIALIZE-INSTANCE
 
-(defmethod shared-initialize :after ((class always-bound-class) slots &key)
-  (let ((gf #'shared-initialize)
-        (specializers (list class (find-class 't)))
-        (qualifiers '(:after)))
-    (a:when-let ((method (find-method gf qualifiers specializers nil)))
-      (remove-method gf method))
-    (let ((method (make-instance 'standard-method
-                                 :specializers specializers
-                                 :qualifiers qualifiers
-                                 :lambda-list '(instance slot-names &rest args)
-                                 :function *always-bound-initialize-mf*)))
-      (add-method gf method))))
+;;; REINITIALIZE-INSTANCE is specified in a way in which it can never make
+;;; slots unbound - hence no implementation is needed.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; UPDATE-INSTANCE-FOR-REDEFINED-CLASS
+
+(defmethod update-instance-for-redefined-class :around
+    ((instance always-bound)
+     added-slots discarded-slots
+     property-list
+     &rest initargs)
+  ;; If a slot already exists or is removed, we do not need to care about it.
+  ;; If a slot is added, then we must ensure that it is going to be bound
+  ;; or signal an error otherwise.
+  (let* ((class (class-of instance))
+         (slots (class-slots class)))
+    (dolist (slot-name added-slots)
+      (let ((slotd (find slot-name slots :key #'slot-definition-name)))
+        (block continue
+          ;; Does a provided initarg match this slot?
+          (let ((slot-initargs (slot-definition-initargs slotd)))
+            (dolist (slot-initarg slot-initargs)
+              (when (get-properties initargs (list slot-initarg))
+                (return-from continue))))
+          ;; No initarg. Does the slot have an initfunction?
+          (let ((initfunction (slot-definition-initfunction slotd)))
+            (when initfunction
+              (return-from continue)))
+          ;; No initarg, no initfunction - this slot will be unbound if we
+          ;; proceed. Ensure it is bound before continuing.
+          (ensure-slot-bound instance slotd)))))
+  (call-next-method))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; UPDATE-INSTANCE-FOR-DIFFERENT-CLASS
+
+;; (defparameter *always-bound-initialize-mf*
+;;   (compile nil (make-method-lambda
+;;                 (class-prototype (find-class 'standard-generic-function))
+;;                 (class-prototype (find-class 'standard-method))
+;;                 `(lambda (instance slot-names &rest args)
+;;                    (declare (ignore slot-names args))
+;;                    (ensure-slots-bound instance))
+;;                 nil)))
+
+;; (defmethod shared-initialize :after ((class always-bound-class) slots &key)
+;;   (let ((gf #'shared-initialize)
+;;         (specializers (list class (find-class 't)))
+;;         (qualifiers '(:after)))
+;;     (a:when-let ((method (find-method gf qualifiers specializers nil)))
+;;       (remove-method gf method))
+;;     (let ((method (make-instance 'standard-method
+;;                                  :specializers specializers
+;;                                  :qualifiers qualifiers
+;;                                  :lambda-list '(instance slot-names &rest args)
+;;                                  :function *always-bound-initialize-mf*)))
+;;       (add-method gf method))))
--- t/always-bound-class.lisp
+++ t/always-bound-class.lisp
@@ -4,20 +4,36 @@
   ((slot :initarg :slot))
   (:metaclass vs:always-bound-class))
 
-(define-test always-bound-class :parent value-semantics-utils
-  (fail (make-instance 'test-always-bound-class) 'unbound-slot)
+(define-test always-bound-class :parent value-semantics-utils)
+
+(define-test always-bound-class-fail :parent always-bound-class
+  (fail (make-instance 'test-always-bound-class) 'unbound-slot))
+
+(define-test always-bound-class-use-value :parent always-bound-class
   (let (instance)
     (handler-bind ((unbound-slot (lambda (c) (use-value 42 c))))
       (setf instance (make-instance 'test-always-bound-class))
-      (is = 42 (slot-value instance 'slot))))
+      (is = 42 (slot-value instance 'slot)))))
+
+(define-test always-bound-class-store-value :parent always-bound-class
   (let (instance)
     (handler-bind ((unbound-slot (lambda (c) (store-value 42 c))))
-      (setf instance (make-instance 'test-always-bound-class)))
+      (setf instance (make-instance 'test-always-bound-class))
+      (is = 42 (slot-value instance 'slot)))))
+
+(define-test always-bound-class-setf-slot-value :parent always-bound-class
+  (let ((instance (make-instance 'test-always-bound-class :slot 42)))
     (is = 42 (slot-value instance 'slot))
     (setf (slot-value instance 'slot) 24)
-    (is = 24 (slot-value instance 'slot))
+    (is = 24 (slot-value instance 'slot))))
+
+(define-test always-bound-class-slot-makunbound :parent always-bound-class
+  (let ((instance (make-instance 'test-always-bound-class :slot 42)))
     (handler-bind ((unbound-slot (lambda (c) (store-value 0 c))))
       (slot-makunbound instance 'slot))
-    (#-sbcl progn
-     #+sbcl skip #+sbcl "https://bugs.launchpad.net/sbcl/+bug/1956621"
-     (is = 0 (print (slot-value instance 'slot))))))
+    (is = 0 (slot-value instance 'slot))))
+
+(define-test always-bound-class-reinitialize-instance :parent always-bound-class
+  (let ((instance (make-instance 'test-always-bound-class :slot 42)))
+    (reinitialize-instance instance :slot 24)
+    (is = 24 (slot-value instance 'slot))))
