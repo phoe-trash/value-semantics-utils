095c8660adb846e4c2f8f04a4c6146c702544f23
Michał "phoe" Herda <phoe@disroot.org>
2022-01-07 19:54:06 +0100

    Add test always-bound-class-update-instance-for-redefined-class

--- t/always-bound-class.lisp
+++ t/always-bound-class.lisp
@@ -37,3 +37,37 @@
   (let ((instance (make-instance 'test-always-bound-class :slot 42)))
     (reinitialize-instance instance :slot 24)
     (is = 24 (slot-value instance 'slot))))
+
+(define-test always-bound-class-update-instance-for-redefined-class
+  :parent always-bound-class
+  (unwind-protect
+       (progn
+         (let (instance)
+           (defclass test-always-bound-class-u-i-f-r-c ()
+             ((slot-1 :initarg :slot-1))
+             (:metaclass vs:always-bound-class))
+           (setf instance (make-instance 'test-always-bound-class-u-i-f-r-c
+                                         :slot-1 42))
+           (defclass test-always-bound-class-u-i-f-r-c ()
+             ((slot-1 :initarg :slot-1)
+              (slot-2 :initarg :slot-2))
+             (:metaclass vs:always-bound-class))
+           (fail (slot-value instance 'slot-1) unbound-slot)
+           (fail (slot-value instance 'slot-1) unbound-slot)
+           (defclass test-always-bound-class-u-i-f-r-c ()
+             ((slot-1 :initarg :slot-1)
+              (slot-2 :initarg :slot-2 :initform 24))
+             (:metaclass vs:always-bound-class))
+           (is = 42 (slot-value instance 'slot-1))
+           (is = 24 (slot-value instance 'slot-2))
+           (defclass test-always-bound-class-u-i-f-r-c ()
+             ((slot-1 :initarg :slot-1)
+              (slot-2 :initarg :slot-2 :initform 24)
+              (slot-3 :initarg :slot-3))
+             (:metaclass vs:always-bound-class))
+           (handler-bind ((unbound-slot (lambda (c) (store-value 0 c))))
+             (slot-value instance 'slot-1))
+           (is = 42 (slot-value instance 'slot-1))
+           (is = 24 (slot-value instance 'slot-2))
+           (is = 0 (slot-value instance 'slot-3))))
+    (setf (find-class 'test-always-bound-class-u-i-f-r-c nil) nil)))
